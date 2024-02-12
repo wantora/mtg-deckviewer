166eb3d9650ccce9e494566648cc90d682993471
wantora <wantora@gmail.com>
2024-02-13 01:08:10 +0900

    Initial commit

---
+++ scripts/generate-data.js
@@ -0,0 +1,154 @@
+"use strict";
+
+const fs = require("node:fs");
+const https = require("node:https");
+const path = require("node:path");
+const {Readable} = require("node:stream");
+const {pipeline} = require("node:stream/promises");
+const StreamArray = require("stream-json/streamers/StreamArray");
+
+function checkLegal(legalities) {
+  for (const legality of Object.values(legalities)) {
+    if (legality !== "not_legal") {
+      return true;
+    }
+  }
+  return false;
+}
+
+function parseTypeLine(typeLine) {
+  return typeLine
+    .match(/^[\w ]+/)[0]
+    .trimEnd()
+    .split(/ +/);
+}
+
+function getColor(cardObject) {
+  if (parseTypeLine(cardObject.type_line).includes("Land")) {
+    return cardObject.color_identity;
+  } else {
+    if (cardObject.card_faces) {
+      return cardObject.card_faces[0].colors;
+    } else {
+      return cardObject.colors;
+    }
+  }
+}
+
+function getImage(cardObject) {
+  if (cardObject.image_uris) {
+    return cardObject.image_uris.small;
+  } else if (cardObject.card_faces && cardObject.card_faces[0].image_uris) {
+    return cardObject.card_faces[0].image_uris.small;
+  }
+  return null;
+}
+
+function oracleCardsParser() {
+  const cards = [];
+  const cardNames = {};
+
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  const stream = new WritableStream({
+    write(chunk) {
+      const cardObject = chunk.value;
+
+      if (!checkLegal(cardObject.legalities)) {
+        return;
+      }
+
+      const card = {
+        name: cardObject.name,
+        cmc: cardObject.cmc,
+        type: parseTypeLine(cardObject.type_line),
+        color: getColor(cardObject),
+        uri: cardObject.scryfall_uri,
+        image: getImage(cardObject),
+      };
+      cards.push(card);
+      const index = cards.length - 1;
+
+      if (cardObject.card_faces) {
+        cardNames[cardObject.card_faces[0].name] = index;
+      } else {
+        cardNames[cardObject.name] = index;
+      }
+    },
+    close() {
+      resolve({cards, cardNames});
+    },
+    abort() {
+      reject();
+    },
+  });
+
+  return {stream, promise};
+}
+
+function httpGet(uri) {
+  return new Promise((resolve, reject) => {
+    https.get(uri, (res) => {
+      res.setEncoding("utf8");
+      let rawData = "";
+      res.on("data", (chunk) => {
+        rawData += chunk;
+      });
+      res.on("end", () => {
+        resolve(rawData);
+      });
+    });
+  });
+}
+
+async function cacheHttpGet(uri) {
+  const cacheFile = path.join("cache", path.basename(uri));
+
+  if (fs.existsSync(cacheFile)) {
+    return fs.createReadStream(cacheFile);
+  } else {
+    fs.mkdirSync(path.dirname(cacheFile), {recursive: true});
+
+    const res = await new Promise((resolve, reject) => {
+      https.get(uri, (r) => {
+        resolve(r);
+      });
+    });
+
+    const streams = Readable.toWeb(res).tee();
+    pipeline(streams[0], fs.createWriteStream(cacheFile));
+    return streams[1];
+  }
+}
+
+(async () => {
+  let oracleCardsUri;
+
+  const metadata = JSON.parse(
+    await httpGet("https://api.scryfall.com/bulk-data")
+  );
+
+  for (const data of metadata.data) {
+    if (data.type === "oracle_cards") {
+      oracleCardsUri = data.download_uri;
+    }
+  }
+
+  const oracleCards = oracleCardsParser();
+
+  await pipeline(
+    await cacheHttpGet(oracleCardsUri),
+    StreamArray.withParser(),
+    oracleCards.stream
+  );
+
+  const cardData = await oracleCards.promise;
+  const outputFile = "src/data.json";
+
+  fs.mkdirSync(path.dirname(outputFile), {recursive: true});
+  fs.writeFileSync(outputFile, JSON.stringify(cardData));
+})();
---
+++ src/index.js
@@ -0,0 +1,157 @@
+import {
+  createApp,
+  ref,
+  computed,
+} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.18/vue.esm-browser.prod.min.js";
+
+function checkLand(type) {
+  if (type.includes("Land")) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function getColorOrder(color) {
+  if (color.length === 0) {
+    return 100;
+  } else {
+    return color.length;
+  }
+}
+
+const COLOR_NAMES = ["W", "U", "B", "R", "G"];
+function getColorNameOrder(colorName) {
+  const index = COLOR_NAMES.indexOf(colorName);
+  if (index === -1) {
+    return 100;
+  } else {
+    return index;
+  }
+}
+
+function compareCards(a, b) {
+  const land = checkLand(a.data.type) - checkLand(b.data.type);
+  if (land !== 0) {
+    return land;
+  }
+  const cmc = a.data.cmc - b.data.cmc;
+  if (cmc !== 0) {
+    return cmc;
+  }
+  const color = getColorOrder(a.data.color) - getColorOrder(b.data.color);
+  if (color !== 0) {
+    return color;
+  }
+  const colorName =
+    getColorNameOrder(a.data.color[0]) - getColorNameOrder(b.data.color[0]);
+  if (colorName !== 0) {
+    return colorName;
+  }
+  if (a.data.name < b.data.name) {
+    return -1;
+  }
+  if (a.data.name > b.data.name) {
+    return 1;
+  }
+  return 0;
+}
+
+function getCardData(str, cardData) {
+  const index = cardData.cardNames[str.replace(/ *\/{1,2} *.*$/, "")];
+  if (index === undefined) {
+    return {
+      name: str,
+      cmc: 0,
+      type: [],
+      color: [],
+      uri: null,
+      image: null,
+    };
+  } else {
+    return cardData.cards[index];
+  }
+}
+
+function parseDecklist(src, cardData) {
+  const sections = [];
+
+  for (const sectionSrc of src.trim().split(/\n{2,}/)) {
+    const lines = sectionSrc
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line !== "");
+    const section = {
+      cards: null,
+      name: null,
+    };
+    const cards = new Map();
+
+    if (
+      lines[0] === "Companion" ||
+      lines[0] === "Commander" ||
+      lines[0] === "Deck" ||
+      lines[0] === "Sideboard"
+    ) {
+      section.name = lines.shift();
+    } else if (sections.every((sec) => sec.name !== "Deck")) {
+      section.name = "Deck";
+    } else {
+      section.name = "Sideboard";
+    }
+
+    for (const line of lines) {
+      const m = line.match(/^(\d+)\s+(.*?)(?:\s+\(\w+\)\s+\d+)?$/);
+      const count = m ? parseInt(m[1], 10) : 1;
+      const name = m ? m[2] : line;
+      const data = getCardData(name, cardData);
+
+      const card = cards.get(data.name);
+      if (card) {
+        card.count += count;
+      } else {
+        cards.set(data.name, {count, data});
+      }
+    }
+    section.cards = Array.from(cards.values()).sort(compareCards);
+
+    sections.push(section);
+  }
+
+  return sections;
+}
+
+function getBackgroundImage(card) {
+  const image = card.data.image;
+  if (image) {
+    return {backgroundImage: `url(${image})`};
+  } else {
+    return {};
+  }
+}
+
+createApp({
+  setup() {
+    const decklist = ref("");
+    const deck = computed(() => {
+      if (cardData.value) {
+        return parseDecklist(decklist.value, cardData.value);
+      } else {
+        return [];
+      }
+    });
+    const cardData = ref(null);
+
+    (async () => {
+      const res = await fetch("data.json");
+      cardData.value = await res.json();
+    })();
+
+    return {
+      decklist,
+      deck,
+      cardData,
+      getBackgroundImage,
+    };
+  },
+}).mount("#app");

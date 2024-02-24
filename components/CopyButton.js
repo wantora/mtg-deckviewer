2b25ea069bfaac8d28d68ea57de20bc7a73ad41a
wantora <wantora@gmail.com>
2024-02-24 20:01:58 +0900

    Updates

--- components/CopyButton.js
+++ components/CopyButton.js
@@ -1,16 +1,19 @@
 import {ref} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.18/vue.esm-browser.prod.min.js";
 
 export default {
-  setup() {
+  setup(props) {
     const showBalloon = ref(false);
 
     async function copyText(text) {
       await navigator.clipboard.writeText(text);
       showBalloon.value = true;
 
-      setTimeout(() => {
-        showBalloon.value = false;
-      }, 1000);
+      setTimeout(
+        () => {
+          showBalloon.value = false;
+        },
+        1000 + props.message.length * 50
+      );
     }
 
     return {
@@ -20,11 +23,12 @@ export default {
   },
   props: {
     text: String,
+    message: String,
   },
   template: `
   <button @click="copyText(text)"><slot>Copy</slot></button>
   <transition name="BalloonMessage">
-    <span v-if="showBalloon" class="BalloonMessage">Copied!</span>
+    <span v-if="showBalloon" class="BalloonMessage">{{ message }}</span>
   </transition>
   `,
 };
